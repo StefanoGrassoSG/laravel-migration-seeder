0a880cc8c5a6263074c3748041cdb8b36d36ef35
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-09-12 17:33:16 +0200

    ADD print data in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,46 @@
     </h1>
     <div class="container">
         <div class="row">
-           
+           @foreach ($trains as $item)
+               <div class="card">
+                    <div class="card-body">
+                        <h2>
+                            Company : {{ $item->company }}
+                        </h2>
+                        <h3>
+                            Departure Station: {{ $item->departure_station }} --- Arrival Station: {{ $item->arrival_station }}
+                        </h3>
+                        <h4>
+                            Departure Time: {{ $item->Departure_time }} --- Arrival Time: {{ $item->arrival_time }}
+                        </h4>
+                        <h4>
+                            Train Type: {{ $item->train_type }}
+                        </h4>
+                        <h4>
+                            In Time:
+                            @php
+                                if($item->in_time == true) {
+                                    echo 'yes';
+                                }
+                                else {
+                                    echo 'no';
+                                }   
+                            @endphp
+                        </h4>
+                        <h4>
+                            Deleted:
+                            @php
+                                if($item->deleted == false) {
+                                    echo 'no';
+                                }
+                                else {
+                                    echo 'yes';
+                                }   
+                            @endphp
+                        </h4>
+                    </div>
+               </div>
+           @endforeach
         </div>
     </div>
 @endsection
